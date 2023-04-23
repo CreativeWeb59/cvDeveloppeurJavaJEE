054addff6177d2d29b9260dea010169fb9534351
CreativeWeb59 <delcri@free.fr>
2023-04-23 16:58:42 +0200

    maj portoflio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 const nav = document.querySelector('.nav')
+const navCote = document.getElementById("menu-cote");
+
 window.addEventListener('scroll', fixNav)
 
 function fixNav(){
-    if(window.scrollY > nav.offsetHeight + 200){
+    if((window.scrollY > nav.offsetHeight + 200) && (screen.width >= 1024)){
         // nav.classList.add('active')
+        navCote.style.display="block";
     } else {
-        // nav.classList.remove('active')
+        navCote.style.display="none";
     }
 }

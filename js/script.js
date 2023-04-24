d7bbf31f964eaf7f9fa11484a264bb22c0f9d6b6
CreativeWeb59 <delcri@free.fr>
2023-04-24 13:24:38 +0200

    reglage css portofolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/* Affichage  de la barre de navigation de coté */
+/* uniquement mode écran */
+
 const nav = document.querySelector('.nav')
 const navCote = document.getElementById("menu-cote");
 
@@ -10,4 +13,31 @@ function fixNav(){
     } else {
         navCote.style.display="none";
     }
+}
+
+/* Gestion du défilement de la page, effet : "smooth" */
+
+/* tableau qui contient les liens de la barre NAV */
+const lienMenu = document.querySelectorAll('nav ul li');
+
+
+/* tableau qui contient les écrans de ma page */
+ecranAffiche = document.querySelectorAll('.ecran');
+
+for (let i=0; i<lienMenu.length; i++) {
+    lienMenu[i].addEventListener('click',
+        function(){positionnerEcran(i);});
+        lienMenu[i].addEventListener('mouseover', function(){changerCurseur(i);});
+}
+
+
+/* tableau qui contient les écrans de ma page */
+ecranAffiche = document.querySelectorAll('.ecran');
+// Fonctions
+function positionnerEcran(numEcran) {
+    ecranAffiche[numEcran].scrollIntoView(
+        {block: 'end',
+        inline: 'start',
+        behavior: 'smooth'}
+    );
 }
